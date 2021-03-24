802f8559f2cf04a89f97c052da1c66a4da24c16f
Douglas Soule <souleman13@gmail.com>
2021-03-24 17:43:51 -0600

    change listen port

--- server.js
+++ server.js
@@ -96,6 +96,6 @@ MongoClient(
 
         //start server
         app.listen(4000, () =>
-            console.log('Example app listening on port 3000!'),
+            console.log('Example app listening on port 4000!'),
         );
     })
