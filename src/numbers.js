b3bcc58bb310c57ed92940afd0816fb1620be8fe
Douglas Soule <souleman13@gmail.com>
2021-03-24 11:13:17 -0600

    toArabicNumeral testing

--- src/numbers.js
+++ src/numbers.js
@@ -1,11 +1,22 @@
 
 const conversionMap = { "M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1 }
 
-const toArabicNumeral = async (startVal) => {
-    //error checking
-    //make all letters uppercase to match convertion map
+const isRoman = (string) => {
+    // regex pattern
+    const pattern = /^(M{1,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})|M{0,4}(CM|C?D|D?C{1,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})|M{0,4}(CM|CD|D?C{0,3})(XC|X?L|L?X{1,3})(IX|IV|V?I{0,3})|M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|I?V|V?I{1,3}))$/
+    return pattern.test(string);
+};
 
-    const array = startVal.split('')
+const toArabicNumeral = async (startVal) => {
+    //all letters uppercase
+    const romanNumeral = startVal.toUpperCase()
+    //error checking
+    if (
+        //only includes valid roman numeral letters
+        !isRoman(romanNumeral)
+        //other error checks...
+        ) return { error: 'input is not a valid roman numeral below 4000' }
+    const array = romanNumeral.split('')
 
     let total = 0
     let current
@@ -16,7 +27,7 @@ const toArabicNumeral = async (startVal) => {
     for (let i = 0; i < array.length; i++) {
         current = array[i]
         currentVal = conversionMap[current]
-        next = array[i+1]
+        next = array[i + 1]
         nextVal = conversionMap[next]
 
         if (currentVal < nextVal) {
--- src/numbers.test.js
+++ src/numbers.test.js
@@ -6,13 +6,24 @@ describe('toArabicNumeral', () => {
     test('IX', async () => {
         expect(await toArabicNumeral('IX')).toBe(9);
     });
-    // test('III', async () => {
-    //     expect(await toArabicNumeral(17)).toBe(17);
-    // });
-    // test('lowercase letters', async () => {
-    //     expect(await toArabicNumeral(17)).toBe(17);
-    // });
+    test('III', async () => {
+        expect(await toArabicNumeral('III')).toBe(3);
+    });
+    test('MMMCMXCIX', async () => { //max roman numeral based on conversion
+        expect(await toArabicNumeral('MMMCMXCIX')).toBe(3999);
+    });
+    test('lowercase letters', async () => {
+        expect(await toArabicNumeral('iii')).toBe(3);
+    });
     //error cases
+    test('input includes invalid letter', async () => {
+        const result = await toArabicNumeral('IMXYIM')
+        expect(result.error).toBe('input is not a valid roman numeral below 4000')
+    });
+    test('input is not a valid roman numeral', async () => {
+        const result = await toArabicNumeral('IMXIM')
+        expect(result.error).toBe('input is not a valid roman numeral below 4000');
+    });
 })
 
 describe('toRomanNumeral', () => {
